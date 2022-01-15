175875d2d240ab5d6a24bd1b54b2cf2c0b89812e
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-15 02:32:58 +0000

    hasta ejercicio 12

--- exercises/11-Add-li-on-Click/index.js
+++ exercises/11-Add-li-on-Click/index.js
@@ -1,5 +1,7 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	let elem = document.createElement("LI");
+	elem.innerHTML= "Forth Element";
+	document.querySelector("#myList ").appendChild(elem);
 });
