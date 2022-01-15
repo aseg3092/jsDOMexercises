99fd988ae40e34a898863b40dae6b1c46e269f9e
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-15 00:13:33 +0000

    hasta ejercicio 8

--- exercises/02-Select-DOM-Element/index.js
+++ exercises/02-Select-DOM-Element/index.js
@@ -1,1 +1,4 @@
 //your code here
+let h1 = document.querySelector("h1");
+
+alert(h1.id);
--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -1,1 +1,5 @@
 // Your code here
+let elem = document.createElement("p");
+elem.innerHTML = " hola a todos";
+elem.style.background = "yellow";
+document.body.appendChild(elem);
