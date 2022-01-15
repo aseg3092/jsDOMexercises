ceb5a569c5ff26a3f492d24de70ec0bbd36b146c
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-15 03:26:39 +0000

    solo falta ejercicio #14

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -3,4 +3,15 @@ window.onload = function() {
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
   // your code here
+  for(let i=0;i< countries.length;i++)
+  {
+	  let elem = document.createElement("option");
+	  elem.text= countries[i];
+	  elem.value=i;
+	  document.querySelector("#mySelect").options.add(elem);
+  }
+  document.getElementById('mySelect').addEventListener('change', function() {
+	alert('You selected: '+this.selectedOptions[0].text);
+  });
+
 };
