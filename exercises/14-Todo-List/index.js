3cbb9f622a6e710b244385a1ed179ca058f3c64b
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-15 05:49:33 +0000

    haciendo todoList

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,22 @@
 // Your code here
+let entrada = document.getElementById("addToDo");
+entrada.addEventListener('change',function(){
+    
+    let elem = document.createElement("LI");
+    
+    let elem2 = document.createElement("SPAN");
+    let elem3 = document.createElement("i");
+    
+    elem3.classList.value="fa fa-trash";
+    elem2.appendChild(elem3);
+    elem.appendChild(elem2);
+    elem.append(" "+entrada.value);
+    document.querySelector("ul").append(elem);
+    entrada.value="";   
+})
+
+let icon = document.querySelectorAll("i");
+icon.addEventListener('click',function(){
+    console.log("funciona");
+})
+
